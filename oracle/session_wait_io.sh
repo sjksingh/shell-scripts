b79e3743f7a1324dd878388f23ab5983a9344376
Sanjeev Singh <sjksingh@gmail.com>
2020-06-19 12:36:12 -0700

    added session-wait and process script

---
+++ oracle/session_wait_io.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sqlplus -s << EOF
+SELECT a.username, a.sql_id, b.object_name, b.object_type, a.event
+FROM v\$session a
+,dba_objects b
+,v\$event_name c
+WHERE b.object_id = a.row_wait_obj#
+AND a.event = c.name
+AND c.wait_class = 'User I/O';
+EOF
